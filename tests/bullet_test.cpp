f1b57c97335499fc2fece49bccebc1807d21c9e0
Ilya <dolinsky94@yandex.ru>
2016-10-24 01:48:10 +0300

    Commit 4

--- tests/bullet_test.cpp
+++ tests/bullet_test.cpp
@@ -6,13 +6,38 @@
 TEST(bullet_test, bullet_construction)
 {
   Bullet bullet1;
-  EXPECT_EQ(bullet1.GetPosition(), Box2D(Point2D{ 0.0f, 0.0f }, Point2D{ 1.0f, 1.0f }));
-  EXPECT_EQ(bullet1.GetOrigin(), Point2D( 0.0f, 0.0f ));
-  EXPECT_EQ(bullet1.GetDirection(), Point2D( 1.0f, 0.0f ));
+  EXPECT_EQ(bullet1.GetPosition(), Box2D(Point2D{ 0.0f, 0.0f }, Point2D{ 0.1f, 0.1f }));
+  EXPECT_EQ(bullet1.GetOrigin(), Point2D(0.0f, 0.0f));
+  EXPECT_EQ(bullet1.GetDirection(), Point2D(1.0f, 0.0f));
 
-  Bullet bullet2 = { Point2D (5.0f, 5.0f), Point2D (1.0f, 0.0f) };
-  EXPECT_EQ(bullet2.GetPosition(), Box2D(Point2D{ 4.5f, 4.5f }, Point2D{ 5.5f, 5.5f }));
+  Bullet bullet2 = { Point2D(5.0f, 5.0f), Point2D(1.0f, 0.0f) };
+  EXPECT_EQ(bullet2.GetPosition(), Box2D(Point2D{ 4.9f, 4.8f }, Point2D{ 5.1f, 5.2f }));
   EXPECT_EQ(bullet2.GetOrigin(), Point2D(5.0f, 5.0f));
   EXPECT_EQ(bullet2.GetDirection(), Point2D(1.0f, 0.0f));
+}
 
+TEST(bullet_test, bullet_set_get)
+{
+  Bullet bullet1;
+  bullet1.SetPosition(Box2D{ 1.0f, 1.0f, 2.0f, 2.0f });
+  EXPECT_EQ(bullet1.GetPosition(), Box2D(Point2D{ 1.0f, 1.0f }, Point2D{ 2.0f, 2.0f }));
+  bullet1.SetOrigin(Point2D{ 1.0f, 1.0f });
+  EXPECT_EQ(bullet1.GetOrigin(), Point2D(1.0f, 1.0f));
+  bullet1.SetDirection(Point2D{ 1.0f, 0.0f });
+  EXPECT_EQ(bullet1.GetDirection(), Point2D(1.0f, 0.0f));
+  bullet1.SetSpeed(1.0f);
+  EXPECT_EQ(bullet1.GetSpeed(), 1.0f);
+  bullet1.SetLength(5.0f);
+  EXPECT_EQ(bullet1.GetLength(), 5.0f);
+  bullet1.SetWidth(10.0f);
+  EXPECT_EQ(bullet1.GetWidth(), 10.0f);
+}
+
+TEST(bullet_test, bullet_move)
+{
+  Bullet bullet1 = { Point2D(5.0f, 5.0f), Point2D(1.0f, 0.0f) };
+  bullet1.Move();
+  EXPECT_EQ(bullet1.GetPosition(), Box2D(Point2D{ 5.9f, 4.8f }, Point2D{ 6.1f, 5.2f }));
+  EXPECT_EQ(bullet1.GetOrigin(), Point2D(6.0f, 5.0f));
+  EXPECT_EQ(bullet1.GetDirection(), Point2D(1.0f, 0.0f));
 }
--- tests/gun_test.cpp
+++ tests/gun_test.cpp
@@ -17,8 +17,8 @@ TEST(gun_test, gun_construction)
 
 TEST(gun_test, test_move_constructor)
 {
-  Gun gun1 = {15};
-  Gun gun2 = {20};
+  Gun gun1 = { 15 };
+  Gun gun2 = { 20 };
   gun2 = std::move(gun1);
   EXPECT_EQ(gun2, 15);
   EXPECT_EQ(gun1, 20);
@@ -30,6 +30,17 @@ TEST(gun_test, test_move_constructor)
 TEST(gun_test, test_shot)
 {
   Gun gun1 = { 20 };
-  gun1.Shot(Point2D{1.0f, 0.0f});
+  gun1.Shot();
   EXPECT_EQ(gun1.GetAmmo(), 19);
+}
+
+TEST(gun_test, gun_set_get)
+{
+  Gun gun;
+  gun.SetPosition(Box2D{ 1.0f, 1.0f, 2.0f, 2.0f });
+  EXPECT_EQ(gun.GetPosition(), Box2D(Point2D{ 1.0f, 1.0f }, Point2D{ 2.0f, 2.0f }));
+  gun.SetAmmo(3);
+  EXPECT_EQ(gun.GetAmmo(), 3);
+  gun.SetDirection(Point2D{ 1.0f, 0.0f });
+  EXPECT_EQ(gun.GetDirection(), Point2D(1.0f, 0.0f));
 }
